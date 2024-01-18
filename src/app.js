01bf97a037a37999dacbe00a5b585bbac2b09abe
josema-git <152654420+josema-git@users.noreply.github.com>
2024-01-18 17:57:11 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+  document.querySelector("#excusa").innerHTML = `${
+    who[Math.floor(Math.random() * who.length)]
+  } ${action[Math.floor(Math.random() * action.length)]} ${
+    what[Math.floor(Math.random() * what.length)]
+  } ${when[Math.floor(Math.random() * when.length)]}`;
   console.log("Hello Rigo from the console!");
 };
